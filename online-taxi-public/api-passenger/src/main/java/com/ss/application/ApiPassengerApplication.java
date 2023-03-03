22a8f6825483afbc5834fb0e041265810c783b63
520ljs <2798377812@qq.com>
2023-03-03 22:12:05 +0800

    27.5、补充

--- online-taxi-public/api-passenger/src/main/java/com/ss/application/ApiPassengerApplication.java
+++ online-taxi-public/api-passenger/src/main/java/com/ss/application/ApiPassengerApplication.java
@@ -1,16 +1,12 @@
-package com.mashibing.apipassenger;
+package com.ss.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients
 public class ApiPassengerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiPassengerApplication.class);
+        SpringApplication.run(ApiPassengerApplication.class, args);
     }
 }
--- online-taxi-public/api-passenger/src/main/java/com/ss/application/controller/TestController.java
+++ online-taxi-public/api-passenger/src/main/java/com/ss/application/controller/TestController.java
@@ -1,8 +1,20 @@
 package com.ss.application.controller;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Auther: ljy.s
  * @Date: 2023/3/3 - 03 - 03 - 22:06
  */
+@RestController
 public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test api passenger";
+    }
+
 }
