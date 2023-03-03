307523df5e294e20602fb799911e1fdc7b913ac3
520ljs <2798377812@qq.com>
2023-03-03 22:57:39 +0800

    28、获取验证码整体流程

---
+++ online-taxi-public/api-passenger/src/main/java/com/ss/application/controller/VerificationCodeController.java
@@ -0,0 +1,29 @@
+package com.ss.application.controller;
+
+import com.ss.application.request.VerificationCodeDTO;
+import com.ss.application.service.VerificationCodeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: ljy.s
+ * @Date: 2023/3/3 - 03 - 03 - 22:32
+ */
+@RestController
+public class VerificationCodeController {
+
+    @Autowired
+    private VerificationCodeService verificationCodeService;
+
+    @GetMapping("/verification-code")
+    public String verificationCode(@RequestBody VerificationCodeDTO verificationCodeDTO) {
+
+        String passengerPhone = verificationCodeDTO.getPassengerPhone();
+        System.out.println("接收到的手机号参数：" + passengerPhone);
+        return verificationCodeService.generatorCode(passengerPhone);
+
+    }
+
+}
---
+++ online-taxi-public/api-passenger/src/main/java/com/ss/application/request/VerificationCodeDTO.java
@@ -0,0 +1,18 @@
+package com.ss.application.request;
+
+/**
+ * @Auther: ljy.s
+ * @Date: 2023/3/3 - 03 - 03 - 22:34
+ */
+public class VerificationCodeDTO {
+
+    private String passengerPhone;
+
+    public String getPassengerPhone() {
+        return passengerPhone;
+    }
+
+    public void setPassengerPhone(String passengerPhone) {
+        this.passengerPhone = passengerPhone;
+    }
+}
