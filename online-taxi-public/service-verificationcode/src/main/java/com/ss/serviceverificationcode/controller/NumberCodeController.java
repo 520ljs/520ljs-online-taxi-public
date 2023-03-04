c26b7da3e0832587c073aa5504c67b9008d64c05
520ljs <2798377812@qq.com>
2023-03-04 16:57:37 +0800

    34、根据数字大小生成验证码的核心代码编写

--- online-taxi-public/service-verificationcode/src/main/java/com/ss/serviceverificationcode/controller/NumberCodeController.java
+++ online-taxi-public/service-verificationcode/src/main/java/com/ss/serviceverificationcode/controller/NumberCodeController.java
@@ -15,15 +15,28 @@ public class NumberCodeController {
     @GetMapping("/numberCode/{size}")
     public String numberCode(@PathVariable("size") int size) {
         System.out.println("size：" + size);
+        // 生成验证码
+        // 获取随机数
+        double mathRandom = (Math.random() * 9 + 1) * (Math.pow(10, size - 1));
+        System.out.println(mathRandom);
+        int resultInt = (int) mathRandom;
+        System.out.println(resultInt);
 
         JSONObject result = new JSONObject();
         result.put("code", 1);
         result.put("message", "success");
         JSONObject data = new JSONObject();
-        data.put("numberCode", 123456);
+        data.put("numberCode", resultInt);
         result.put("data", data);
 
         return result.toString();
     }
 
+    public static void main(String[] args) {
+        // 获取随机数
+        /*double mathRandom = (Math.random() * 9 + 1) * (Math.pow(10, 5));
+        System.out.println(mathRandom);
+        int resultInt = (int) mathRandom;
+        System.out.println(resultInt);*/
+    }
 }
