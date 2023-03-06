75891a3ee9cf9d7ae457110911f05de0190873e6
520ljs <2798377812@qq.com>
2023-03-06 22:24:32 +0800

    55、乘客用户服务注册到nacos

--- online-taxi-public/service-passenger-user/src/main/java/com/ss/servicepassengeruser/ServicePassengerUserApplication.java
+++ online-taxi-public/service-passenger-user/src/main/java/com/ss/servicepassengeruser/ServicePassengerUserApplication.java
@@ -4,8 +4,10 @@ import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.ss.servicepassengeruser.mapper")
 public class ServicePassengerUserApplication {
 
