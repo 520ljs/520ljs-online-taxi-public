c3257cc90582fd15a1ba3ac39a7bfca379bf235f
520ljs <2798377812@qq.com>
2023-03-31 09:44:38 +0800

    100、service-price调用service-map

---
+++ online-taxi-public/service-price/src/main/java/com/ss/serviceprice/remote/ServiceMapClient.java
@@ -0,0 +1,21 @@
+package com.ss.serviceprice.remote;
+
+import com.ss.internalcommon.dto.ResponseResult;
+import com.ss.internalcommon.request.ForecastPriceDTO;
+import com.ss.internalcommon.response.DirectionResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @Auther: ljy.s
+ * @Date: 2023/3/30 - 03 - 30 - 17:31
+ */
+@FeignClient("service-map")
+public interface ServiceMapClient {
+
+    @RequestMapping(method = RequestMethod.GET,value = "/direction/driving")
+    public ResponseResult<DirectionResponse> direction(@RequestBody ForecastPriceDTO forecastPriceDTO);
+
+}
--- online-taxi-public/service-price/src/main/java/com/ss/serviceprice/service/ForecastPriceService.java
+++ online-taxi-public/service-price/src/main/java/com/ss/serviceprice/service/ForecastPriceService.java
@@ -1,8 +1,12 @@
 package com.ss.serviceprice.service;
 
 import com.ss.internalcommon.dto.ResponseResult;
+import com.ss.internalcommon.request.ForecastPriceDTO;
+import com.ss.internalcommon.response.DirectionResponse;
 import com.ss.internalcommon.response.ForecastPriceResponse;
+import com.ss.serviceprice.remote.ServiceMapClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,6 +17,9 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class ForecastPriceService {
 
+    @Autowired
+    private ServiceMapClient serviceMapClient;
+
     public ResponseResult forecastPrice(String depLongitude, String depLatitude, String destLongitude, String destLatitude) {
 
         log.info("出发地经度：" + depLongitude);
@@ -21,6 +28,16 @@ public class ForecastPriceService {
         log.info("目的地维度：" + destLatitude);
 
         log.info("调用地图服务，查询距离和时长");
+        // 自己的参数自己来，每个接口参数单独封装一下
+        ForecastPriceDTO forecastPriceDTO = new ForecastPriceDTO();
+        forecastPriceDTO.setDepLongitude(depLongitude);
+        forecastPriceDTO.setDepLatitude(depLatitude);
+        forecastPriceDTO.setDestLongitude(destLongitude);
+        forecastPriceDTO.setDestLatitude(destLatitude);
+        ResponseResult<DirectionResponse> direction = serviceMapClient.direction(forecastPriceDTO);
+        Integer distance = direction.getData().getDistance();
+        Integer duration = direction.getData().getDuration();
+        log.info("距离：" + distance + "，时长：" + duration);
 
         log.info("读取计价规则");
 
