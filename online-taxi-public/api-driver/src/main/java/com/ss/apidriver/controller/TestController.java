718bd1d461522ebb897c262bf83d0c057d70c505
520ljs <2798377812@qq.com>
2023-04-10 14:53:26 +0800

    136、api-driver服务创建

---
+++ online-taxi-public/api-driver/src/main/java/com/ss/apidriver/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.ss.apidriver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author:ljy.s
+ * @Date:2023/4/10 - 04 - 10 - 14:50
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "api-driver";
+    }
+
+}
