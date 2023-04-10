2bb814a0477b00904010bef97d3fe09c57c1bf77
520ljs <2798377812@qq.com>
2023-04-10 14:39:12 +0800

    135、api-boss调用service-driver-user实现司机信息修改
    之前忘提交一次，现在补上：
    132、api-boss接口设计

--- online-taxi-public/api-boss/src/main/java/com/ss/apiboss/controller/DriverUserController.java
+++ online-taxi-public/api-boss/src/main/java/com/ss/apiboss/controller/DriverUserController.java
@@ -5,6 +5,7 @@ import com.ss.internalcommon.dto.DriverUser;
 import com.ss.internalcommon.dto.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,12 @@ public class DriverUserController {
     public ResponseResult addDriverUser(@RequestBody DriverUser driverUser) {
 
         return driverUserService.addDriverUser(driverUser);
+    }
+
+    @PutMapping("/driver-user")
+    public ResponseResult updateDriverUser(@RequestBody DriverUser driverUser) {
+
+        return driverUserService.updateDriverUser(driverUser);
     }
 
 }
