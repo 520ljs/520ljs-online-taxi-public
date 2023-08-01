2b6f21385fc19ed4b885ae9c01657d8b03abef9a
520ljs <2798377812@qq.com>
2023-08-01 15:31:16 +0800

    235、司机客户端模拟，项目搭建

---
+++ online-taxi-public/sse-driver-client-web/src/test/java/com/ss/ssedriverclientweb/SseDriverClientWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.ssedriverclientweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseDriverClientWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
